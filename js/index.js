ecead71d86ebc3171c309d36088731a91e68531b
khalid saaf <khallid.saaf@gmail.com>
2021-05-22 23:09:12 +0100

    responsive tablet

--- js/index.js
+++ js/index.js
@@ -1,62 +1,69 @@
-ScrollReveal().reveal('.item1', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item2', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item3', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
 
-ScrollReveal().reveal('.item4', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item5', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
+// // alert("page label")
 
-ScrollReveal().reveal('.item6', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item7', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item8', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item9', {
-    delay: 375,
-    duration: 500,
-    reset: true
-});
-ScrollReveal().reveal('.item10', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item11', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
-ScrollReveal().reveal('.item12', {
-    delay: 300,
-    duration: 200,
-    reset: true
-});
+
+
+// //animation section2
+
+// ScrollReveal().reveal('.item1', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item2', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item3', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+
+// ScrollReveal().reveal('.item4', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item5', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+
+// ScrollReveal().reveal('.item6', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item7', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item8', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item9', {
+//     delay: 375,
+//     duration: 500,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item10', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item11', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });
+// ScrollReveal().reveal('.item12', {
+//     delay: 300,
+//     duration: 200,
+//     reset: true
+// });

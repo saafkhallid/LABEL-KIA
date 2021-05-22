54796875abc27757d3b73c6f34d39cc3962e0362
khalid saaf <khallid.saaf@gmail.com>
2021-05-22 16:46:07 +0100

    faire animation section2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,62 @@
-alert("page label kia")
+ScrollReveal().reveal('.item1', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item2', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item3', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
 
+ScrollReveal().reveal('.item4', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item5', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
 
+ScrollReveal().reveal('.item6', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item7', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item8', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item9', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item10', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item11', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item12', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});

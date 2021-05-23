ee046eb77ebc1358f5e64ba2ca259fa5c1794472
khalid saaf <khallid.saaf@gmail.com>
2021-05-23 14:31:40 +0100

    modification section2

--- js/index.js
+++ js/index.js
@@ -1,69 +1,72 @@
 
-// // alert("page label")
+// alert("page label")
 
 
 
-// //animation section2
+//animation section2
 
-// ScrollReveal().reveal('.item1', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item2', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item3', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
+ScrollReveal().reveal('.item1', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item2', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item3', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
 
-// ScrollReveal().reveal('.item4', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item5', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
+ScrollReveal().reveal('.item4', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item5', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
 
-// ScrollReveal().reveal('.item6', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item7', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item8', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item9', {
-//     delay: 375,
-//     duration: 500,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item10', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item11', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
-// ScrollReveal().reveal('.item12', {
-//     delay: 300,
-//     duration: 200,
-//     reset: true
-// });
+ScrollReveal().reveal('.item6', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item7', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item8', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item9', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+ScrollReveal().reveal('.item10', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item11', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+ScrollReveal().reveal('.item12', {
+    delay: 300,
+    duration: 200,
+    reset: true
+});
+
+
+//animation hero header 
